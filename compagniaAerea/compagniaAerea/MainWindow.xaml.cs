0df78c504e8504f86c0ef6aeb518e003c62fe2a6
Mattia Rovinelli <rovi92@hotmail.it>
2016-02-29 19:33:59 +0100

    Main base del nostro progetto.Scarno che sarà poi dda modificare esteticamente ma come soluzione ottima,non ci sono i btn indietro che implementeremo poi.

---
+++ compagniaAerea/compagniaAerea/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace compagniaAerea
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void dipendente_click(object sender, System.EventArgs e)
+        {
+            sinistra.Visibility = Visibility.Hidden;
+            destra.Visibility = Visibility.Hidden;
+            gridDipendente.Visibility = Visibility.Visible;
+
+        }
+
+        private void cliente_click(object sender, RoutedEventArgs e)
+        {
+            sinistra.Visibility = Visibility.Hidden;
+            destra.Visibility = Visibility.Hidden;
+            cliente.Visibility = Visibility.Visible;
+        }
+    }
+}
