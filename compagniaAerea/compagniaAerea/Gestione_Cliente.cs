31c699c65ca43af2a020756d54fdb89699433fd3
Mattia Rovinelli <rovi92@hotmail.it>
2016-03-14 17:00:52 +0100

    commit per cecca

--- compagniaAerea/compagniaAerea/Gestione_Cliente.cs
+++ compagniaAerea/compagniaAerea/Gestione_Cliente.cs
@@ -74,45 +74,6 @@ namespace compagniaAerea
             //Console.WriteLine("VEDIAMO COSA SCRIVE STA ROBA "+prova.ToString());
         }
 
-        public bool erroriDiScrittura(String nome, String cognome, DateTime data_di_nascita, String username, String password, String password2, String indirizzo, String telefono, String email, String stato, String regione, String città, int CAP, String CF)
-        {
-            //metodo per correggere i campi 
-           
-            //per ora ho fatto solo l'inserimento dei dati in una array list in maniera che sia piena di true e folse e che le nostre textbox siano legate a un numero identificativo da 1 a... 
-            //così facendo gli si può applicare un metodo ciclico che restituisce dove è l'errore e cosa contiene
-            String ddNascita = Convert.ToString(data_di_nascita);
-            String psw = Convert.ToString(password);
-            String psw2 = Convert.ToString(password2);
-            #region nome
-            alErrori.Add(correctNull(nome,"nome"));
-            alErrori.Add(correctNull(cognome,"cognome"));
-            alErrori.Add(correctNull(ddNascita, "data di nascita"));
-            alErrori.Add(correctNull(username, "username"));
-           //password e password2 devono avere un altro tipo di correzzione cioè un metodo per la lunghezza
-           //indirizzo non ha nessun tipo di controllo può essere vuoto
-           //telefono ha bisogno del controllo numerico e di lunghezza massima
-           //e mail non ha bisogno di controllo sui campi ma ha bisogno del controllo dell'esistenza 
-            alErrori.Add(correctNull(stato, "stato"));
-            alErrori.Add(correctNull(regione, "regione"));
-            alErrori.Add(correctNull(città, "città"));
-            //il cap e il CF hanno bisogno di avere un controllo di lunghezza come telefono quindi li farò appena posso
-            #endregion
-
-
-
-            return true;
-        }
-        public Boolean correctNull(String val,String stVal)
-        {
-            if (val != "")
-            {
-                if (val != stVal)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
         public Boolean statoRegistrazioneCliente()
         {
             return this.statoRegistrazione;
